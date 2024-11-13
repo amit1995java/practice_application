c3da1347acc6a65a6c05ab417282a264b8c021a5
AmitKumar0609 <102433544+AmitKumar0609@users.noreply.github.com>
2024-11-13 11:09:41 +0530

    Added change for hello java
    Added change for hello java

--- src/main/java/com/practice/test/PracticeTest.java
+++ src/main/java/com/practice/test/PracticeTest.java
@@ -3,7 +3,8 @@ package com.practice.test;
 public class PracticeTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Hello Java!");
 
 	}
 
