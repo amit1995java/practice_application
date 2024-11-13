249db3b4634792c290d169234a5af61cad42a61c
AmitKumar0609 <102433544+AmitKumar0609@users.noreply.github.com>
2024-11-13 12:29:22 +0530

    practice program for anagram
    practice program for anagram

--- src/main/java/com/practice/test/PracticeTest.java
+++ src/main/java/com/practice/test/PracticeTest.java
@@ -1,10 +1,29 @@
 package com.practice.test;
 
+import java.util.Arrays;
+
 public class PracticeTest {
+	public Boolean anagram(String str1,String str2) {
+		//first check length same or not 
+		if(str1.length()!=str2.length()) {
+			return false;
+		}
+		
+		str1=str1.toLowerCase();
+		str2=str2.toLowerCase();
+		char[] chararray1=str1.toCharArray();
+		char[] chararray2=str2.toCharArray();
+		
+		Arrays.sort(chararray1);
+		Arrays.sort(chararray2);
+		
+		return Arrays.equals(chararray1, chararray2);
+	}
 
 	public static void main(String[] args) {
-
-		System.out.println("Hello Java!");
+		PracticeTest newobj=new PracticeTest();
+		
+		System.out.println( newobj.anagram("isten", "silent")+" Hello Java!");
 
 	}
 
