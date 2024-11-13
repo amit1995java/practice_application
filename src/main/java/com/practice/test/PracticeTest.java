3b719209ba4c1d165f11143092eb13733ed6807e
AmitKumar0609 <102433544+AmitKumar0609@users.noreply.github.com>
2024-11-13 19:29:16 +0530

    addes some more project
    addes some more project

--- src/main/java/com/practice/test/PracticeTest.java
+++ src/main/java/com/practice/test/PracticeTest.java
@@ -1,8 +1,18 @@
 package com.practice.test;
-
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Arrays;
 
 public class PracticeTest {
+	//Implement a function to check if two strings are rotations of each other.
+	public Boolean isRotation(String s1, String s2) {
+		if(s1.length()!= s2.length())
+			return false;
+		String concatenated = s1+s1;
+		return  concatenated.contains(s1);
+	}
+	
+	// Write a function to count the occurrences of a specific character in a string.
 	public Boolean anagram(String str1,String str2) {
 		//first check length same or not 
 		if(str1.length()!=str2.length()) 
@@ -19,11 +29,68 @@ public class PracticeTest {
 		
 		return Arrays.equals(chararray1, chararray2);
 	}
+	
+	//Write a function to count the occurrences of a specific character in a string.
+	public static int countOccurence(String str,char target) {
+		int count =0;
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)==target) count++;
+		}
+		return count;
+	}
+	
+	//Write a function to find all permutations of a string.
+	
+
+	 public static Character findFirstNonRepeatingCharacter(String str) {
+	       // Step 1: Create a LinkedHashMap to store character counts
+	       Map<Character, Integer> charCountMap = new LinkedHashMap<>();
+
+	        // Step 2: Populate the map with character frequencies
+	        for (char c : str.toCharArray()) {
+	            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+	        }
+
+	        // Step 3: Find the first character with a count of 1
+	        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+	            if (entry.getValue() == 1) {
+	                return entry.getKey();
+	            }
+	        }
+
+	        // Step 4: If no non-repeating character is found, return null
+	        return null;
+	    }
+	       
+	    
+	
+
+	
 
 	public static void main(String[] args) {
+		String word = "Blanket";
+		String[] arr = word.split(" ");
+		System.out.println(arr[0]);
 		PracticeTest newobj=new PracticeTest();
 		
 		System.out.println( newobj.anagram("listen", "silent")+" Hello Java!");
+		System.out.println(countOccurence("amitkumark",'k'));
+	
+		// String operation 
+		System.out.println("hello".hashCode());
+		System.out.println("12345".matches("\\d+")); // Output: true
+		
+		System.out.println(newobj.isRotation("abcd","bcda"));
+		
+		
+		String str = "swiss";
+	    Character result = findFirstNonRepeatingCharacter(str);
+	    if (result != null) {
+	    System.out.println("First non-repeating character: " + result);
+	    } else {
+	            System.out.println("No non-repeating character found.");
+	        }
+
 
 	}
 
