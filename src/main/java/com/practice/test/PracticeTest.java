cd539ef1d6448c7ce548ac575fd8af0881f848e1
AmitKumar0609 <102433544+AmitKumar0609@users.noreply.github.com>
2024-11-13 12:45:26 +0530

    Update PracticeTest.java of anagram
    Update PracticeTest.java of anagram

--- src/main/java/com/practice/test/PracticeTest.java
+++ src/main/java/com/practice/test/PracticeTest.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class PracticeTest {
 	public Boolean anagram(String str1,String str2) {
 		//first check length same or not 
-		if(str1.length()!=str2.length()) {
+		if(str1.length()!=str2.length()) 
 			return false;
-		}
+		
 		
 		str1=str1.toLowerCase();
 		str2=str2.toLowerCase();
@@ -23,7 +23,7 @@ public class PracticeTest {
 	public static void main(String[] args) {
 		PracticeTest newobj=new PracticeTest();
 		
-		System.out.println( newobj.anagram("isten", "silent")+" Hello Java!");
+		System.out.println( newobj.anagram("listen", "silent")+" Hello Java!");
 
 	}
 
